c423d0ea0e084b50c33caca21d92f1bcace53e03
AngelRitsuka <88597612+AngelRitsuka@users.noreply.github.com>
2023-05-30 21:05:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,70 +1,64 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var holder,ball,ground;
-var stand1,stand2;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
 
-var slingShot; //corda
 
-var fruit,cesta; //imagens
-var backgroundImg ;
-
-function preload()
-{
-  backgroundImg = loadImage("background.png");
-  fruit=loadImage("melon.png");
-  cesta=loadImage("basket.png")
-}
-
-function setup() 
-{
-  createCanvas(900,400);
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
 
-  Engine.run(engine);
-
-  ground = new Ground(450,390,900,20);
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
   
- 
+ // player = new ( 340, playerBase.position.y - 112, 120, 120);
+ //player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+ // player =  Player( 340, playerBase.position.y - 112, 120, 120);
+ // player= new Player( )
 
-  //Desafio1:
-  ball = Bodies.circle(50,200,20);
-  World.add(world,ball);
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
 
-//Desafio2
-  slingShot = new Slingshot(this.ball,{x:100, y:100}); //corda
-
+  
 }
+
 function draw() {
-  background(backgroundImg); 
- 
+  background(189);
+
   Engine.update(engine);
-  //text(mouseX + ',' + mouseY, 10, 15);
+
+  // Título
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+
+ 
+  playerBase.display();
   
-  ground.display();
-  cesta.scale=.025;
+  //display();
+  //playerdisplay();
+  //player();
+  //player.display();
+  
 
-
-  imageMode(CENTER)
-  image(fruit ,ball.position.x,ball.position.y,40,40);
-  image(cesta,450,270)
-
-  slingShot.display();
+  computerBase.display();
+  computer.display();
+  
 }
-
-//segurar e solta (estilingue)
-function mouseDragged(){
-  Matter.Body.setPosition(this.ball,{x:mouseX, y:mouseY});
-}
-function mouseReleased(){
-  slingShot.fly();
-
-}
-
-
